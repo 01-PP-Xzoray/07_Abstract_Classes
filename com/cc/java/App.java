d9fc9ecfb5572da14f08824be3f183a2470bb06e
Sohrab <sohrab-arenja8@hotmail.de>
2023-08-02 13:00:03 +0200

    abstract

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,9 @@ public class App {
     
     public static void main(String[] args) {
      
-       
+        Rectangle rectangle = new Rectangle(10, 10);
+        output("------ Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
     }
 
   
